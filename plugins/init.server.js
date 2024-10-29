5dbc98d23fa2e5df515f79b48c6c932b8f5367b4
Faisal khan <fskhandev@gmail.com>
2024-10-29 23:20:34 +0500

    testing

--- plugins/init.server.js
+++ plugins/init.server.js
@@ -1,13 +1,17 @@
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-  const token = useCookie("token");
-  const getUser = useUserAuth();
+  const token = useCookie("token", {
+    httpOnly: true, // Make the cookie inaccessible to JavaScript
+    secure: true,   // Ensures the cookie is sent only over HTTPS
+    sameSite: 'Lax' // Set SameSite attribute to 'Lax'
+  });
+const getUser = useUserAuth();
 
-  if (token.value) {
-    await getUser();
-  }
-  console.log("server side token", token.value);
-  
+if (token.value) {
+  await getUser();
+}
+console.log("server side token", token.value);
+
   // if (process.server) {
   //   const headers = useRequestHeaders();
   //   console.log('Incoming Headers on Server:', headers);
